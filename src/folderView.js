1dc2092a59cc6ff13cd53f954a4722b531cb459a
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-03-19 22:19:09 +0000

    init config

--- src/folderView.js
+++ src/folderView.js
@@ -50,9 +50,8 @@ export async function renderFolderView(items, path, request) {
     )
 
   const intro = `<div class="intro markdown-body" style="text-align: left; margin-top: 2rem;">
-                    <h2>Yoo, I'm Spencer Woo 👋</h2>
-                    <p>This is Spencer's OneDrive public directory listing. Feel free to download any files that you find useful. Reach me at: spencer.wushangbo [at] gmail [dot] com.</p>
-                    <p><a href="https://spencerwoo.com">Portfolio</a> · <a href="https://blog.spencerwoo.com">Blog</a> · <a href="https://github.com/spencerwooo">GitHub</a></p>
+                    <h2>Planet Cloud OneDrive Index</h2>
+                    <p><a href="https://planet-cloud.fun">Planet Cloud</a> · <a href="https://github.com/HEIGE-PCloud">GitHub</a> · <a href="https://status.planet-cloud.fun">Service Status</a></p>
                   </div>`
 
   // Check if current directory contains README.md, if true, then render spinner
